a5a11f1f141de0b0907467f01ffce1b9ddd9a2c4
EthernetUser <72755781+EthernetUser@users.noreply.github.com>
2020-11-09 19:20:38 +0300

    Delete connection.php

--- php/connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-
-// подключение к бд
-$connection = mysqli_connect('localhost', 'root', '', 'second_db_magaz');
-// $connection = mysqli_connect('localhost', 'cm66513_user', '74189SASHAs', 'cm66513_user');
-
